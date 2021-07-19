17baee13b259aed19bae20f55d20b33f4892b3a5
S.Ben Stewart <sbenstewart@gmail.com>
2021-07-19 22:55:21 +0530

    made it simpler to understand

--- the-content/auto-kill/auto-kill.js
+++ the-content/auto-kill/auto-kill.js
@@ -1,46 +1,49 @@
 $(document).ready(function(){
 
-function block() {
-  console.log("Trying to censor based on location")
-  var flag = false;
-  $.get("https://api.ipify.org/", function(ipAddress, status){
-    console.log("ipAddress: " + ipAddress + "\nstatus: " + status)
-    if(status == "success") {
-      $.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent('http://ipinfo.io/'+ipAddress+'/json'), function (data) {
-          var country = JSON.parse(data.contents).country
-          console.log("country: " + country);
-          //if country India hide appstore
-          if(country.toUpperCase() != 'in'.toUpperCase()) {
-            console.log('Country is not India')
-            flag = true
-            $(".not-india").removeClass('d-none');
-          } else {
-            $(".india").removeClass('d-none'); 
-          }
-          $('#status').delay(350).fadeOut('slow');
-          $('#preloader').delay(350).fadeOut('slow');
-      });
-    }
+function getData(ajaxurl) { 
+  return $.ajax({
+    url: ajaxurl,
+    type: 'GET',
   });
+};
+
+async function block() {
+  console.log("Trying to censor based on location")
+  var ipAddress = await getData("https://api.ipify.org/");
+  console.log("ipAddress: " + ipAddress)
+  var countryData = await getData('https://api.allorigins.win/get?url=' + encodeURIComponent('http://ipinfo.io/'+ipAddress+'/json'));
+  console.log(countryData);
+  var country = JSON.parse(countryData.contents).country
+  console.log("country: " + country);
+  //if country India hide appstore
+  if(country.toUpperCase() != 'in'.toUpperCase()) {
+    console.log('Country is not India')
+    $(".not-india").removeClass('d-none');
+  } else {
+    console.log('Country is not India')
+    $(".india").removeClass('d-none'); 
+  }
+  $('#status').delay(350).fadeOut('slow');
+  return $('#preloader').delay(350).fadeOut('slow').promise();
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function autoKill() {
+  var promise = block();
+  promise.then(function(){
+    console.log('auto kill start')
+    sleep(7500).then(()=> {
+      alert("Auto killing the page now :(");
+      document.getElementsByTagName('body')[0].remove();
+    })
+  })
 }
 
 $(window).on('load', function () {
-  block()
+  autoKill()
 });
 
 });
-
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-function autoKill(url) {
-  monitoredWindow = window.open(url, '_blank');
-  sleep(6000);
-  console.log("Shutting the website"+ url +" down.");
-  monitoredWindow.close() ;
-}
